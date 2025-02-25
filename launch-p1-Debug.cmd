561b6f9be7541cc0040703460e5a20c2172b7622
kamronu <kubry@ufl.edu>
2025-02-25 15:41:34 -0500

    Add project files.

---
+++ launch-p1-Debug.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/kamro/Desktop/cg_project_1/cg_project_1/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\kamro\Desktop\cg_project_1\cg_project_1\build\Debug\p1.exe"  
+pause
